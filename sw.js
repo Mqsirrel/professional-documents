ec86629763f2eeb4e2fe2ced9f923615b9cfd06b
Mqid <68720462+Mqsirrel@users.noreply.github.com>
2026-08-26 02:54:33 +0300

    fix: bust stale v1 PWA cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'cyb-plan-v1';
+const CACHE = 'cyb-plan-v2';
 const APP_SHELL = ['./','./index.html','./styles.css','./app.js','./data/courses.json','./site.webmanifest','./icons/icon-192.svg','./icons/icon-512.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
