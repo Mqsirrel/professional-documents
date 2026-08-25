60d5994e774eb7d87d17bf53b624ab71bc660700
Mqid <68720462+Mqsirrel@users.noreply.github.com>
2026-08-26 02:42:02 +0300

    feat: add offline service worker

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE = 'cyb-plan-v1';
+const APP_SHELL = ['./','./index.html','./styles.css','./app.js','./data/courses.json','./site.webmanifest','./icons/icon-192.svg','./icons/icon-512.svg'];
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting())));
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    const copy = response.clone(); caches.open(CACHE).then(cache => cache.put(event.request, copy)); return response;
+  }).catch(() => caches.match('./index.html'))));
+});
